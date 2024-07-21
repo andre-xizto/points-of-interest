b8eb5db26f25c46b92189351549f5a3ce91b390e
= <=>
2024-07-21 15:30:26 -0300

    create project

---
+++ src/test/java/dev/buskopan/point_of_interest/PointOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.buskopan.point_of_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
