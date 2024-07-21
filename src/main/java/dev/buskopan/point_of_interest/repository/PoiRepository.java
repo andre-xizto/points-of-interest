aa4ee824190c5ae1bb26c65ca6579cb90e956cd5
= <=>
2024-07-21 17:11:01 -0300

    add function to return POIs within box

--- src/main/java/dev/buskopan/point_of_interest/repository/PoiRepository.java
+++ src/main/java/dev/buskopan/point_of_interest/repository/PoiRepository.java
@@ -2,6 +2,11 @@ package dev.buskopan.point_of_interest.repository;
 
 import dev.buskopan.point_of_interest.model.Poi;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PoiRepository extends JpaRepository<Poi, Long> {
+    @Query(value = "SELECT p FROM Poi p WHERE p.x >= :xMin AND p.x <= :xMax AND p.y >= :yMin AND p.y <= :yMax")
+    List<Poi> findWithinBoundingBox(double xMin, double xMax, double yMin, double yMax);
 }
