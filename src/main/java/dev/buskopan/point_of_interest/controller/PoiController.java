04522fcb9f6c3590585a45cd0a214123b6f6c12b
= <=>
2024-07-21 17:46:39 -0300

    create poi controller

---
+++ src/main/java/dev/buskopan/point_of_interest/controller/PoiController.java
@@ -0,0 +1,28 @@
+package dev.buskopan.point_of_interest.controller;
+
+import dev.buskopan.point_of_interest.controller.dto.PoiDTO;
+import dev.buskopan.point_of_interest.model.Poi;
+import dev.buskopan.point_of_interest.service.PoiService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/create")
+public class PoiController {
+    private final PoiService poiService;
+
+    public PoiController(PoiService poiService) {
+        this.poiService = poiService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody PoiDTO dto) {
+        Poi poi = poiService.create(dto);
+        PoiDTO poiResponse = new PoiDTO(poi.getName(), poi.getX(), poi.getY());
+        return ResponseEntity.status(HttpStatus.CREATED).body(poiResponse);
+    }
+}
