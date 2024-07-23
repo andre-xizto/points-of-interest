a6934ac74656b2c79299487b55285dccadd6f172
= <=>
2024-07-22 22:40:06 -0300

    add /near endpoint to list near POIs

--- src/main/java/dev/buskopan/point_of_interest/controller/PoiController.java
+++ src/main/java/dev/buskopan/point_of_interest/controller/PoiController.java
@@ -1,8 +1,10 @@
 package dev.buskopan.point_of_interest.controller;
 
+import dev.buskopan.point_of_interest.controller.dto.NearRequestDTO;
 import dev.buskopan.point_of_interest.controller.dto.PoiDTO;
 import dev.buskopan.point_of_interest.model.Poi;
 import dev.buskopan.point_of_interest.service.PoiService;
+import jakarta.annotation.Nonnull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +21,23 @@ public class PoiController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody PoiDTO dto) {
+    public ResponseEntity<PoiDTO> create(@RequestBody PoiDTO dto) {
         Poi poi = poiService.create(dto);
         PoiDTO poiResponse = new PoiDTO(poi.getName(), poi.getX(), poi.getY());
         return ResponseEntity.status(HttpStatus.CREATED).body(poiResponse);
     }
 
     @GetMapping("/list")
-    public ResponseEntity<?> listAll () {
+    public ResponseEntity<List<PoiDTO>> listAll() {
         List<Poi> pois = poiService.listAll();
+        List<PoiDTO> poisResponse = new ArrayList<>();
+        pois.forEach(poi -> poisResponse.add(new PoiDTO(poi.getName(), poi.getX(), poi.getY())));
+        return ResponseEntity.status(HttpStatus.OK).body(poisResponse);
+    }
+
+    @PostMapping("/near")
+    public ResponseEntity<List<PoiDTO>> near(@RequestBody NearRequestDTO dto) {
+        List<Poi> pois = poiService.listNear(dto.x(), dto.y(), dto.dMax());
         List<PoiDTO> poisResponse = new ArrayList<>();
         pois.forEach(poi -> poisResponse.add(new PoiDTO(poi.getName(), poi.getX(), poi.getY())));
         return ResponseEntity.status(HttpStatus.OK).body(poisResponse);
